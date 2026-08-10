54495c351b69a41593d3b056f5d30d09b6effffc
LumosMaxima <92662361+LumosMaxima12@users.noreply.github.com>
2026-08-10 13:40:35 +0800

    feat: wire theme and i18n providers

--- apps/desktop/src/main.tsx
+++ apps/desktop/src/main.tsx
@@ -3,14 +3,21 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { AppProviders } from "./app/providers";
 import { router } from "./app/router";
+import { I18nProvider } from "./i18n/I18nProvider";
+import { ThemeProvider } from "./theme/ThemeProvider";
 import "./styles/tokens.css";
+import "./styles/themes.css";
 import "./styles/globals.css";
 import "./styles/visual-polish.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AppProviders>
-      <RouterProvider router={router} />
-    </AppProviders>
+    <ThemeProvider>
+      <I18nProvider>
+        <AppProviders>
+          <RouterProvider router={router} />
+        </AppProviders>
+      </I18nProvider>
+    </ThemeProvider>
   </React.StrictMode>,
 );
